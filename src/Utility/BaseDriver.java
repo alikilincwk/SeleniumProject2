243b1573dfbc0df9e7abc3e5e5d9538f860923a9
ariftemur <temur6@hotmail.com>
2024-09-13 21:40:37 +0200

    Proje oluşturuldu ve class lar açıldı

---
+++ src/Utility/BaseDriver.java
@@ -0,0 +1,28 @@
+package Utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public class BaseDriver {
+    public static WebDriver driver;
+
+    public static Actions actions;
+
+    static {
+        driver = new ChromeDriver();
+        actions=new Actions(driver);
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+
+    }
+
+    public static void tearDown() {
+        MyFunc.wait(3);
+        driver.quit();
+    }
+
+}
