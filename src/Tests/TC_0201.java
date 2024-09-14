5cabe429abb9a6b4b48f88dd1f848f1d30b310db
kardelenssu <kurtkardelensu@gmail.com>
2024-09-14 15:28:15 +0200

    TC_0201 yapildi.

--- src/Tests/TC_0201.java
+++ src/Tests/TC_0201.java
@@ -1,4 +1,85 @@
 package Tests;
 
-public class TC_0201 {
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+public class TC_0201 extends BaseDriver {
+    @Test
+    public void test1() {
+        driver.get("https://demowebshop.tricentis.com/");
+        MyFunc.wait(1);
+
+        WebElement register = driver.findElement(By.xpath("(//div [@class='header-links']//a)[1]"));
+        actions.click(register).build().perform();
+        MyFunc.wait(3);
+
+        WebElement Mail1 = driver.findElement(By.xpath("(//div [@class='inputs']/input)[3]"));
+        actions.click(Mail1).build().perform();
+        actions.sendKeys("testerkurs202401@gmx.com").build().perform();
+        MyFunc.wait(1);
+
+        WebElement RegisterButton1=driver.findElement(By.id("register-button"));
+        actions.click(RegisterButton1).build().perform();
+        MyFunc.wait(1);
+
+        WebElement male = driver.findElement(By.xpath("(//div [@class='gender']/input)[1]"));
+        actions.click(male).build().perform();
+        MyFunc.wait(1);
+
+        WebElement FirstName = driver.findElement(By.xpath("(//div [@class='inputs']/input)[1]"));
+        actions.click(FirstName).build().perform();
+        actions.sendKeys("Techno").build().perform();
+        MyFunc.wait(1);
+
+        WebElement Surname = driver.findElement(By.xpath("(//div [@class='inputs']/input)[2]"));
+        actions.click(Surname).build().perform();
+        actions.sendKeys("Study").build().perform();
+        MyFunc.wait(1);
+
+        WebElement Password = driver.findElement(By.xpath("(//div [@class='inputs']/input)[4]"));
+        actions.click(Password).build().perform();
+        actions.sendKeys("tester01").build().perform();
+        MyFunc.wait(1);
+
+        WebElement PasswordAgain = driver.findElement(By.xpath("(//div [@class='inputs']/input)[5]"));
+        actions.click(PasswordAgain).build().perform();
+        actions.sendKeys("tester01").build().perform();
+        MyFunc.wait(1);
+
+        WebElement RegisterButton=driver.findElement(By.id("register-button"));
+        actions.click(RegisterButton).build().perform();
+        MyFunc.wait(1);
+
+        WebElement ReFirstName = driver.findElement(By.xpath("(//div [@class='inputs']/input)[1]"));
+        actions.click(ReFirstName).build().perform();
+        actions.keyDown(Keys.BACK_SPACE).keyDown(Keys.BACK_SPACE).keyDown(Keys.BACK_SPACE).keyDown(Keys.BACK_SPACE).keyDown(Keys.BACK_SPACE).keyDown(Keys.BACK_SPACE).keyUp(Keys.BACK_SPACE).sendKeys("Ali").build().perform();
+        MyFunc.wait(1);
+
+
+        WebElement ReSurname = driver.findElement(By.xpath("(//div [@class='inputs']/input)[2]"));
+        actions.click(ReSurname).build().perform();
+        actions.keyDown(Keys.BACK_SPACE).keyDown(Keys.BACK_SPACE).keyDown(Keys.BACK_SPACE).keyDown(Keys.BACK_SPACE).keyDown(Keys.BACK_SPACE).keyUp(Keys.BACK_SPACE).sendKeys("Demir").build().perform();
+        MyFunc.wait(1);
+
+        WebElement RegisterButton2=driver.findElement(By.id("register-button"));
+        actions.click(RegisterButton2).build().perform();
+        MyFunc.wait(1);
+
+
+        waitAndQuit();
+
+
+
+
+
+
+
+
+
+
+    }
 }
